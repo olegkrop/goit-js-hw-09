6b01ab3a8087636083d81c9cdfa6744897008c1c
Oleg Kropiviansky <oleg.kropiviansky@gmail.com>
2023-01-27 18:43:41 +0200

    feat: add color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const refs = {
+    start: document.querySelector('[data-start]'),
+    stop: document.querySelector('[data-stop]'),
+};
+let interval = 0;
 
+refs.start.addEventListener('click', onStartButton);
+refs.stop.addEventListener('click', onStopButton);
+
+function onStartButton() {
+    refs.start.setAttribute('disabled', '');
+    interval = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+}
+
+function onStopButton() {
+    refs.start.removeAttribute('disabled');
+    clearInterval(interval);
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
